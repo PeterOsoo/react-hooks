bc920919b2442f9d28def126a340270fd9938bee
Rateng <48685190+PeterOsoo@users.noreply.github.com>
2023-11-19 09:49:02 +0300

    Update CounterFive.js
    arrow function

--- src/components/CounterFive.js
+++ src/components/CounterFive.js
@@ -1,7 +1,7 @@
-import React from "react"
+
 import useCounter from "../hooks/useCounter"
 
-function CounterFive() {
+const CounterFive = () => {
 	const [count, increment, decrement, reset] = useCounter(10, 10)
 
 	return (
